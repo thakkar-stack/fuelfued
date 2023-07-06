36dcf5f364c2b9332463ea2b4cd62afff9df3ce0
Aditya Thakkar <63272852+thakkar-stack@users.noreply.github.com>
2023-07-06 13:29:26 +1000

    rounded off output of values

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 # this is a python based app to provide an easy solution to split the costs of fuel after a trip with mates
 
-def main():
-    trip_info()
-
 def trip_info():
     print('When going on a trip with friends you should start a car trip to measure the distance travelled. When filling up petrol, note the cost of fuel per litre. You should also note your cars litres per 100km fuel economy.')
     fuel_cost = float(input('What was the cost of petrol per litre?:'))
     fuel_economy = float(input('How many litres per 100km does your car use?:'))
     travel_dist = float(input('What was the total distance travelled?:'))
     people = int(input('How many people?:'))
-    petrol_used = fuel_economy*(travel_dist/100)
-    total_price = (petrol_used*fuel_cost)
-    final_price = (petrol_used*fuel_cost)/people
+    petrol_used = round(fuel_economy*(travel_dist/100))
+    total_price = round(petrol_used*fuel_cost)
+    final_price = round((petrol_used*fuel_cost)/people)
     
     print(f'The total amount of fuel used is {petrol_used}L, the cost of fuel is ${total_price} and the cost per person is ${final_price}')
 
-main()
+trip_info()
